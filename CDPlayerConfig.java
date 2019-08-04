65fbfabaefc64362360fb89495b95d6087fda3b5
zhangfangsheng <53328099+zhangfangsheng@users.noreply.github.com>
2019-08-04 09:58:45 +0800

    Add files via upload

---
+++ CDPlayerConfig.java
@@ -0,0 +1,15 @@
+package chp02.autowiring.config;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * @ComponentScan("chp02.autowiring")也可以写成：
+ * @ComponentScan(basePackages="chp02.autowiring");
+ * @ComponentScan(basePackages={"chp02.autowiring"});
+ * 或者，更进一步用basePackageClasses定义某些类所在的包作为扫描包
+ * @ComponentScan(basePackageClasses={CDPlayer.class, DVDPlayer.class});
+ */
+@Configuration
+@ComponentScan("chp02.autowiring")
+public class CDPlayerConfig { 
+}
