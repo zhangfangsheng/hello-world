437fedabb179e3d5acb3e3f899f9cc035fec63d4
zhangfangsheng <53328099+zhangfangsheng@users.noreply.github.com>
2019-08-04 09:59:20 +0800

    Add files via upload

---
+++ test/CDPlayerTest.java
@@ -0,0 +1,44 @@
+package chp02.autowiring.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import chp02.autowiring.CompactDisc;
+import chp02.autowiring.MediaPlayer;
+import chp02.autowiring.config.CDPlayerConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=CDPlayerConfig.class)
+public class CDPlayerTest {
+
+  @Rule
+  public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+  @Autowired
+  private MediaPlayer player;
+  
+  @Autowired
+  private CompactDisc cd;
+  
+  @Test
+  public void cdShouldNotBeNull() {
+    assertNotNull(cd);
+  }
+
+  @Test
+  public void play() {
+    player.play();
+    assertEquals(
+        "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\r\n", 
+        log.getLog());
+  }
+
+}
