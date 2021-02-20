7a693c9f0b635acbccb31c0c71600ba12e65ea43
zhangfangsheng <zfschengyuan@163.com>
2021-02-20 14:48:18 +0800

    bb

--- CDPlayerConfig.java
+++ CDPlayerConfig.java
@@ -8,11 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * @ComponentScan(basePackages={"chp02.autowiring"});
  * 或者，更进一步用basePackageClasses定义某些类所在的包作为扫描包
  * @ComponentScan(basePackageClasses={CDPlayer.class, DVDPlayer.class});
-<<<<<<< HEAD
- * add new commentsabcd
-=======
- * add new commentsabce
->>>>>>> origin/trientry
+ * god
  */
 @Configuration
 @ComponentScan("chp02.autowiring")
